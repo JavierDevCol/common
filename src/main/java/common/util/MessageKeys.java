ba44c0ecf3661f6c74ca2daba7c03abf20049331
pedrolop900 <pedro.lopez@orionsoftware.com.co>
2020-02-21 19:48:59 -0500

    eliminados partes innecesarias.

--- src/main/java/common/util/MessageKeys.java
+++ src/main/java/common/util/MessageKeys.java
@@ -1,14 +1,14 @@
 package common.util;
 
 public interface MessageKeys {
-    String NOT_FOUND = "heystyles.commons.not.found";
-    String NOT_FOUND_OBJECT = "heystyles.commons.not.found.object";
-    String VALIDATION_MESSAGE = "heystyles.commons.validation.message.default";
-    String VALIDATION_INVALID_JSON = "heystyles.commons.validation.json.invalid";
-    String VALIDATION_ENUM_INVALID = "heystyles.commons.validation.enum.invalid";
-    String ERROR_DB_CONNECTION = "heystyles.commons.error.db.connection";
-    String ERROR_DB_CONSTRAINTS = "heystyles.commons.error.db.constraints";
-    String ERROR_UNEXPECTED = "heystyles.commons.error.unexpected";
-    String ERROR_CONVERTER_NOT_FOUND = "heystyles.commons.error.converter.not-found";
+    String NOT_FOUND = "polaris.commons.not.found";
+    String NOT_FOUND_OBJECT = "polaris.commons.not.found.object";
+    String VALIDATION_MESSAGE = "polaris.commons.validation.message.default";
+    String VALIDATION_INVALID_JSON = "polaris.commons.validation.json.invalid";
+    String VALIDATION_ENUM_INVALID = "polaris.commons.validation.enum.invalid";
+    String ERROR_DB_CONNECTION = "polaris.commons.error.db.connection";
+    String ERROR_DB_CONSTRAINTS = "polaris.commons.error.db.constraints";
+    String ERROR_UNEXPECTED = "polaris.commons.error.unexpected";
+    String ERROR_CONVERTER_NOT_FOUND = "polaris.commons.error.converter.not-found";
 }
 
