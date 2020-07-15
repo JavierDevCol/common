d81cbb03ae6bdb80a5eac6d1a6ef05e3de9d5179
pedro <pedrolopez@wposs.com>
2020-07-15 12:22:25 -0500

    solucionados problemas con entity.

--- polaris_cloud_common/src/main/java/common/types/Entity.java
+++ polaris_cloud_common/src/main/java/common/types/Entity.java
@@ -3,7 +3,6 @@ package common.types;
 import java.io.Serializable;
 
 public abstract class Entity<ID extends Serializable> extends IdObject<ID> implements Serializable {
-    private static final long serialVersionUID = -7716070975924354714L;
 
     public Entity() {
     }
